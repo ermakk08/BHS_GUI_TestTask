79326332f4593bce6daef5e45e9cd7471e26a426
Ermakov Sergey <107313590+ermakk08@users.noreply.github.com>
2025-02-23 01:41:33 +0300

    unzipgit

---
+++ AvaloniaApplication3/AvaloniaApplication3/Converters/HalfConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace AvaloniaApplication3.Converters
+{
+    public class HalfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                return d / 2;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
