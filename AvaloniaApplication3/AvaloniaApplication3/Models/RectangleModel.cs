e01442c3e0edff9347622756de68eb33ee6254c1
Ermakov Sergey <107313590+ermakk08@users.noreply.github.com>
2025-02-23 01:41:34 +0300

    unzipgit

---
+++ AvaloniaApplication3/AvaloniaApplication3/Models/RectangleModel.cs
@@ -0,0 +1,29 @@
+﻿namespace AvaloniaApplication3.Models
+{
+    public class RectangleModel : ShapeModel
+    {
+        private double _width = 100;
+        private double _height = 100;
+        private double _rotation = 0;
+        
+        public override string Title => "Прямоугольник";
+
+        public double Width
+        {
+            get => _width;
+            set => SetProperty(ref _width, value);
+        }
+
+        public double Height
+        {
+            get => _height;
+            set => SetProperty(ref _height, value);
+        }
+
+        public double Rotation
+        {
+            get => _rotation;
+            set => SetProperty(ref _rotation, value);
+        }
+    }
+}
