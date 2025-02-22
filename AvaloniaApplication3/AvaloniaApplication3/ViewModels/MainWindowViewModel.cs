5ed3d3d46024031bbc1994c4b317fe226e6d329c
Ermakov Sergey <107313590+ermakk08@users.noreply.github.com>
2025-02-23 01:41:38 +0300

    unzipgit

---
+++ AvaloniaApplication3/AvaloniaApplication3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using AvaloniaApplication3.Models;
+using ReactiveUI;
+
+namespace AvaloniaApplication3.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private ShapeModel? _selectedShape;
+
+    public MainWindowViewModel()
+    {
+        AddRectangleCommand = ReactiveCommand.Create(AddRectangle);
+        AddCircleCommand = ReactiveCommand.Create(AddCircle);
+    }
+
+    public ObservableCollection<ShapeModel> Shapes { get; } = new();
+
+
+    public ShapeModel? SelectedShape
+    {
+        get => _selectedShape;
+        set => this.RaiseAndSetIfChanged(ref _selectedShape, value);
+    }
+
+    public ICommand AddRectangleCommand { get; }
+    public ICommand AddCircleCommand { get; }
+
+    private void AddRectangle()
+    {
+        Shapes.Add(new RectangleModel());
+    }
+
+    private void AddCircle()
+    {
+        Shapes.Add(new CircleModel());
+    }
+}
